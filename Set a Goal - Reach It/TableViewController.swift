3e676122503c3410939d7f330d8789e54739e59b
Salomon serfati <salo1serfati@gmail.com>
2016-06-20 22:38:46 +0200

    Model and Model Controller complete

--- Set a Goal - Reach It/TableViewController.swift
+++ Set a Goal - Reach It/TableViewController.swift
@@ -26,14 +26,6 @@ class TableViewController: UIViewController {
         let nGvc = NewGoalViewController(nibName: "NewGoalViewController", bundle: nil)
         self.navigationController?.pushViewController(nGvc, animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
