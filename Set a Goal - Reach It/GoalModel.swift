2a3336f13aec7d2d0e4886cc299c9b7c420ef6b7
Salomon serfati <salo1serfati@gmail.com>
2016-06-20 22:35:36 +0200

    Model and Model Controller created. Three functions created within the Model Controller.

---
+++ Set a Goal - Reach It/GoalModel.swift
@@ -0,0 +1,39 @@
+//
+//  GoalModel.swift
+//  Set a Goal - Reach It
+//
+//  Created by Salomon serfati on 6/20/16.
+//  Copyright © 2016 Salomon Serfati. All rights reserved.
+//
+
+import Foundation
+
+
+class GoalModel:NSObject { 
+    var title: String?
+    var author: String?
+    var totalPages: Int?
+    var completionTime: Int?
+    var completed: Bool?
+    var currentPage: Int?
+    var pagesPerDay: Int?
+    var dateCreated: NSDate?
+    
+    //Creating  the Object for the new Goal. Parameters are required, what is in the init is optional.
+    init (title:String, author:String, totalPages:Int, completionTime:Int) {
+        //Given by Parameters
+        self.title = title
+        self.author = author
+        self.totalPages = totalPages
+        self.completionTime = completionTime
+        
+        //Additional descriptions of Goal
+        self.completed = false
+        self.currentPage = 0
+        self.pagesPerDay = 0
+        self.dateCreated = NSDate()
+        
+        
+    }
+    
+}
---
+++ Set a Goal - Reach It/GoalModelController.swift
@@ -0,0 +1,63 @@
+//
+//  GoalModelController.swift
+//  Set a Goal - Reach It
+//
+//  Created by Salomon serfati on 6/20/16.
+//  Copyright © 2016 Salomon Serfati. All rights reserved.
+//
+
+import Foundation
+
+
+class GoalModelController:NSObject {
+    //create goals array (array of objects)
+    var goalList: [GoalModel] = []
+    
+    // Singleton design pattern
+    class var sharedInstance: GoalModelController {
+        struct Static {
+            static var instance:GoalModelController?
+            static var token: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.token)    {
+            Static.instance = GoalModelController()
+        }
+        return Static.instance!
+    }
+
+    //function that creates a new Goal
+    func createNewGoal(title:String, author:String, totalPages:Int, completionTime:Int) {
+        //create new goal and append to goalList
+        let newGoal = GoalModel(title: title, author: author, totalPages: totalPages, completionTime: completionTime)
+        newGoal.completed = false
+        newGoal.currentPage = 0
+        newGoal.pagesPerDay = self.checkPagesPerDay(totalPages, completionTime: completionTime)
+        newGoal.dateCreated = NSDate()
+        print("\(newGoal.dateCreated)")
+        goalList.append(newGoal)
+    }
+    
+    //gets goals from goalList
+    func getGoalList()->[GoalModel] {
+        if goalList.isEmpty{
+            print("The goalList is empty and the getGoalList funcion was called")
+        }
+        return goalList
+        
+    }
+    
+    //calculates the pages needed to be read per day AT THE START OF THE GOAL
+    func checkPagesPerDay(totalPages:Int, completionTime:Int) -> Int {
+        let totalPages = totalPages
+        let completionTime = completionTime
+        let pagesPerDay = totalPages / completionTime
+        
+        return pagesPerDay
+    
+    }
+    
+    
+    
+    
+}
